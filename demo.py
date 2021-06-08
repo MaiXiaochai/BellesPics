fcd8964d67a3b47999755e2278be8b44418ec482
you <you@example.com>
2021-06-09 07:36:00 +0800

    feat:demo.py

---
+++ demo.py
@@ -0,0 +1,14 @@
+# -*- encoding: utf-8 -*-
+
+"""
+------------------------------------------
+@File       : demo.py
+@Author     : maixiaochai
+@Email      : maixiaochai@outlook.com
+@CreatedOn  : 2021/6/6 23:34
+------------------------------------------
+"""
+# requests 中携带 cookie
+# 1. 直接在浏览器中找到cookie相关信息，手动添加到 headers中
+# 2. requests.session
+
